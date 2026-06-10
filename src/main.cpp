73aa5dbed0fa1add8682089061e1c693aa9ab3d5
Dodoroshka <yanovaanastasia18@gmail.com>
2026-06-10 12:52:01 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main() {
             train.getLength();
             opRandom = train.getOpCount();
         }
-        std::cout << n << "\t" << opFalse << 
+        std::cout << n << "\t" << opFalse <<
             "\t\t" << opTrue << "\t\t" << opRandom << "\n";
     }
     return 0;
